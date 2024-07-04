63dff2b73c4891aac2cced60bc71627865a0f639
SENEOArgb <162048770+SENEOArgb@users.noreply.github.com>
2024-07-04 11:00:20 +0300

    Создание классов Subject, SubjectControl. Заполнение Subject

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Subjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
